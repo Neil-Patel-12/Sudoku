93d270b61943fe447d1487dab39a6586991376b0
Andrew Huang <andrew03huang@gmail.com>
2023-04-18 11:53:24 -0400

    made cell class

---
+++ cell.py
@@ -0,0 +1,15 @@
+import pygame
+
+class Cell:
+    def __init__(self, value, row, col, screen):
+        self.value = value
+        self.row = row
+        self.col = col
+        self.screen = screen
+        self.sketch = 0
+
+    def set_cell_value(self, value):
+        self.value = value
+
+    def set_sketched_value(self, value):
+        self.sketch = value
