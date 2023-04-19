e8c14ddd7c59cfe34c7dc4db6b45a0bd4e82f7c3
Srsi3 <127258769+Srsi3@users.noreply.github.com>
2023-04-18 20:11:56 -0400

    Created board class

---
+++ board.py
@@ -0,0 +1,27 @@
+import pygame
+class Board:
+    def __init__(self):
+        pass
+    def draw(self):
+        pass
+    def select(self, row col):
+    pass
+
+    def click(self, x, y):
+        pass
+    def clear(self):
+        pass
+    def sketch(self, value):
+        pass
+    def place_number(self, value):
+        pass
+    def reset_to_original(self):
+        pass
+    def is_full(self):
+        pass
+    def update_board(self):
+        pass
+    def find_empty(self):
+        pass
+    def check_board(self):
+        pass
