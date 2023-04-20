c0073ffb15a23698df2444c80dd402fc8e486104
kianambrose <kianambrose04@gmail.com>
2023-04-20 14:21:50 -0400

    Added some code to a few of the functions of the class board.py

--- board.py
+++ board.py
@@ -11,25 +11,43 @@ class Board:
 
         pass
     def draw(self):
-        pass
+        # I think this may need more detail
+        for row in self.grid:
+            for cell in row:
+                cell.draw()
     def select(self, row, col):
         pass
 
     def click(self, x, y):
-        pass
+        if x < 0 or y < 0 or x > self.width * 50 or y > self.height * 50:
+            return None
+        row = y // 50
+        col = x // 50
+        return (row, col)
     def clear(self):
         pass
     def sketch(self, value):
-        pass
+        if self.selected:
+            self.grid[self.selected[0]][self.selected[1]].sketch = value
     def place_number(self, value):
-        pass
+        if self.selected:
+            cell = self.grid[self.selected[0]][self.selected[1]]
+            if not cell.original:
+                cell.value = value
+                cell.sketch = None
     def reset_to_original(self):
         pass
     def is_full(self):
         pass
     def update_board(self):
-        pass
+        for row in self.grid:
+            for cell in row:
+                cell.update_value()
     def find_empty(self):
-        pass
+        for row in range(9):
+            for col in range(9):
+                if self.grid[row][col].value == 0:
+                    return (row, col)
+        return None
     def check_board(self):
         pass
